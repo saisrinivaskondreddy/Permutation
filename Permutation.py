6e124ed31db161b8c4ab030e52e6ee5aace12e10
saisrinivaskondreddy <saisrinivas.chowdary@gmail.com>
2016-10-19 17:07:04 +0300

    19-Oct-2016 : Removed duplicates

--- Permutation.py
+++ Permutation.py
@@ -5,7 +5,6 @@ myString=[]
 myString, myNum=first_input.split(" ")
 ret_list=[]
 my_Dummy='a'
-#my_Dummy=" "
 
 def factorial(n):
     #while i > 0:
@@ -20,45 +19,36 @@ def factorial(n):
 
 def permutation(a, myString):  #  a - dummy in case of first call , b - n distinct objects taken r at a time 
 	i=0
-	print ("DEBUG : in-side function permutations %d times" %i)
-	print "my string.. recursion : %s" %myString
+	#print ("DEBUG : in-side function permutations %d times" %i)
+	#print "my string.. recursion : %s" %myString
 	temp_list_2=[]
 	if len(myString) == 2:
-		print "DEBUG : in-side function permutations - String Length 2"
+		#print "DEBUG : in-side function permutations - String Length 2"
 		temp=[myString[0]+myString[1], myString[1]+myString[0]]
-		#print temp
 		return temp
-		#temp_list_2.append(temp)
 	elif len(myString) == 3:
-		print "DEBUG : in-side function permutations - String Length 3"
+		#print "DEBUG : in-side function permutations - String Length 3"
 		temp=[myString[0]+myString[1]+myString[2], myString[0]+myString[2]+myString[1], myString[1]+myString[0]+myString[2], myString[1]+myString[2]+myString[0], myString[2]+myString[1]+myString[0], myString[2]+myString[0]+myString[1] ]
-		#print temp
 		return temp
 	elif len(myString) > 3:
-			print ("DEBUG : in-side function permutations - String Length MORE %d " % len(myString))
+			#print ("DEBUG : in-side function permutations - String Length MORE %d " % len(myString))
 			myString_length_temp=len(myString)
 			temp_list_2=[]
 		#while  myString_length_temp >= 1:
 			#print ("will call permutation of %c, %s" %(myString[i] myString[0:i]+myString[i+1:]) )
-			print ("will call permutation of %c with" %myString[i])
-			print ("will call permutation of %s" %myString[0:i])
-			print ("will call permutation of %s" %myString[i+1:])
-			print ("will call permutation of %s" %(myString[0:i]+myString[i+1:]) )
+			#print ("will call permutation of %c with" %myString[i])
+			#print ("will call permutation of %s" %myString[0:i])
+			#print ("will call permutation of %s" %myString[i+1:])
+			#print ("will call permutation of %s" %(myString[0:i]+myString[i+1:]) )
 			temp_list_2=permutation(myString[i], myString[0:i]+myString[i+1:])
 			temp_list_3=merge_lists(myString[i], temp_list_2);
-			print "Going to print temp_list_3 *****************"
+			#print "Going to print temp_list_3 *****************"
 			#print temp_list_3
-			print "Going to print temp_list_3 *****************"
+			#print "Going to print temp_list_3 *****************"
 			ret_list.append(temp_list_3)
 			#ret_list.append(permutation(myString[i], myString[0:i]+myString[i:]))
-			#temp_list_2=permutation(myString[i], myString[0:i]+myString[i:])
-			#print ("values of : %s %s %s" %(myString[i] myString[0:i] myString[i:])")
-			#print myString[i] myString[0:i] myString
 			myString_length_temp -= 1
 			i += 1
-			#print ("Intermediate values of i : %d and myString_length_temp : %d" %(i, myString_length_temp))
-			#print ("Intermediate values of i : %d" %i)
-			#print temp_list_2
 			return temp_list_3
 	#return temp_list_3
 
@@ -79,13 +69,16 @@ def merge_lists(x, list_b): #process [x] [abc, bcd]
 #		print "======="
         return ret_list_2
 
+def remove_duplicate(myList):
+	return list(set(myList))
+
 #comb( myString );
 #lst_input = ['xyz', 'tuv']
 #final_list=merge_lists('a', ['xyz', 'tuv']);
 final_list=permutation(my_Dummy, myString)
-#ret_list.sort()
-#print ret_list
-length=len(final_list)
+final_list_1=remove_duplicate(final_list)
+final_list_1.sort()
+length=len(final_list_1)
 print "****** Number of permutations : %d" %length
-print final_list
+print final_list_1
 
