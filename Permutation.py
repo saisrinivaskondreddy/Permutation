46cf36e7c8eb6fdaba03ae6c0475e3ea58d453ab
saisrinivaskondreddy <saisrinivas.chowdary@gmail.com>
2016-10-21 15:21:10 +0300

    Final Draft will full functionality 	1. Removed hardcoding 	2. Added nPr functionality 	3. Added input validations

--- Permutation.py
+++ Permutation.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python
 
+import sys
+import numbers
+from compiler.ast import flatten
+
 first_input=raw_input("")
 myString=[]
 myString, myNum=first_input.split(" ")
+ReqNum=int(myNum)
 ret_list=[]
 my_Dummy='a'
 
+if ReqNum > len(myString):
+	print "Please check your input, seems something wrong"
+	print "Enter the string and required number.. "
+	print "Ex:- abc 3"
+	print "Ex:- Output will print all permutations of abc"
+	print "Output:- 'abc', 'acb', 'bac', 'bca', 'cab', 'cba'"
+	sys.exit(1)
+
+
 def factorial(n):
-    #while i > 0:
-    #    n = n * n-1
     if n==0:
         return 1
     elif n==1:
@@ -19,66 +31,51 @@ def factorial(n):
 
 def permutation(a, myString):  #  a - dummy in case of first call , b - n distinct objects taken r at a time 
 	i=0
-	#print ("DEBUG : in-side function permutations %d times" %i)
-	#print "my string.. recursion : %s" %myString
 	temp_list_2=[]
-	if len(myString) == 2:
-		#print "DEBUG : in-side function permutations - String Length 2"
+	if ReqNum == 1:
+		temp=list(myString)
+		return temp
+	elif len(myString) == 2 and len(myString) <= ReqNum:
 		temp=[myString[0]+myString[1], myString[1]+myString[0]]
 		return temp
-	elif len(myString) == 3:
-		#print "DEBUG : in-side function permutations - String Length 3"
-		temp=[myString[0]+myString[1]+myString[2], myString[0]+myString[2]+myString[1], myString[1]+myString[0]+myString[2], myString[1]+myString[2]+myString[0], myString[2]+myString[1]+myString[0], myString[2]+myString[0]+myString[1] ]
-		return temp
-	elif len(myString) > 3:
-			#print ("DEBUG : in-side function permutations - String Length MORE %d " % len(myString))
-			myString_length_temp=len(myString)
-			temp_list_2=[]
-		#while  myString_length_temp >= 1:
-			#print ("will call permutation of %c, %s" %(myString[i] myString[0:i]+myString[i+1:]) )
-			#print ("will call permutation of %c with" %myString[i])
-			#print ("will call permutation of %s" %myString[0:i])
-			#print ("will call permutation of %s" %myString[i+1:])
-			#print ("will call permutation of %s" %(myString[0:i]+myString[i+1:]) )
+	elif len(myString) > 2:
+		myString_length_temp=len(myString)
+		temp_list_2=[]
+		temp_list_4=[]
+		while  myString_length_temp >= 1:
+			#print "calling permutation(%s, %s)" %(myString[i], myString[0:i]+myString[i+1:])
 			temp_list_2=permutation(myString[i], myString[0:i]+myString[i+1:])
-			temp_list_3=merge_lists(myString[i], temp_list_2);
-			#print "Going to print temp_list_3 *****************"
-			#print temp_list_3
-			#print "Going to print temp_list_3 *****************"
+			temppp=flatten(temp_list_2)
+			temp_list_3=merge_lists(myString[i], temppp);
+			temp_list_4.append(temp_list_3)
 			ret_list.append(temp_list_3)
-			#ret_list.append(permutation(myString[i], myString[0:i]+myString[i:]))
 			myString_length_temp -= 1
 			i += 1
-			return temp_list_3
-	#return temp_list_3
+		return temp_list_4
 
 def merge_lists(x, list_b): #process [x] [abc, bcd]
         ret_list_2=[]
         for data in range(len(list_b)):
                 temp_data=list_b[data] #consider each element of the list, EX:- abc
-		#print "data lenthh : %d" % len(temp_data)
                 for j in range(len(temp_data)+1):
-                        if j == 0:
-				#print ("j is : %d" %j)
-				#print (x+temp_data)
+                        if j == 0 and len(temp_data) < ReqNum:
                                 ret_list_2.append(x+temp_data)
-                        elif j > 0 and j <= len(temp_data):
-				 #print ("j is : %d" %j)
-				 #print (temp_data[0:j]+x+temp_data[j:])
-                                 ret_list_2.append(temp_data[0:j]+x+temp_data[j:])
-#		print "======="
+			elif j == 0 and len(temp_data) == ReqNum:
+				ret_list_2.append(temp_data)
+                        elif j > 0 and j <= len(temp_data) and len(temp_data) < ReqNum:
+                                ret_list_2.append(temp_data[0:j]+x+temp_data[j:])
+			elif j > 0 and j <= len(temp_data) and len(temp_data) == ReqNum:
+				ret_list_2.append(temp_data)
         return ret_list_2
 
 def remove_duplicate(myList):
 	return list(set(myList))
 
-#comb( myString );
-#lst_input = ['xyz', 'tuv']
-#final_list=merge_lists('a', ['xyz', 'tuv']);
 final_list=permutation(my_Dummy, myString)
-final_list_1=remove_duplicate(final_list)
-final_list_1.sort()
-length=len(final_list_1)
+final_list_1=flatten(final_list)
+final_list_2=remove_duplicate(final_list_1)
+final_list_2.sort()
+length=len(final_list_2)
 print "****** Number of permutations : %d" %length
-print final_list_1
+print final_list_2
 
